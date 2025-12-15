d263f6b4bf1e2c112958b5001c7d6fe7605859ec
fortune_cookie <leon.gao.dev@outlook.com>
2025-12-15 21:50:33 +0100

    feat(server): update prisma version to 7

--- apps/server/src/lib/prisma.ts
+++ apps/server/src/lib/prisma.ts
@@ -1,18 +1,10 @@
-import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
-  });
-};
+const connectionString = `${process.env.DATABASE_URL}`;
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-}
-
-const prisma = globalThis.prisma ?? prismaClientSingleton();
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
